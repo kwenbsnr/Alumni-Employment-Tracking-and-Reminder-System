d320f34009081feff55cd1f8ff4fd02b14c2a1c2
Quien Bisnar <bisnar.quien18@gmail.com>
2025-10-21 15:52:39 +0800

    Change database connection to use port 3307

--- connect.php
+++ connect.php
@@ -4,7 +4,8 @@ $user = "root";
 $pass = "";            
 $db   = "alumtrak_experiment"; 
 
-$conn = new mysqli($host, $user, $pass, $db);
+//$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli("127.0.0.1:3307", "root", "", "alumtrak_experiment");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
